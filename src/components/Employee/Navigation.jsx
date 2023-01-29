2c113289d3d8fede8cdcb753b4309d1a898f4c9c
Adam Anderson <adamcanderson400@gmail.com>
2023-01-29 09:34:16 -0500

    fixed scroll issue on home page

--- src/components/Employee/Navigation.jsx
+++ src/components/Employee/Navigation.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Navigation({email}) {
     return (
         <div className="flex flex-col font-semibold text-3xl gap-6 w-fit align-right items-end mt-6">
-            <Link href="/employee" className="w-fit hover:text-primary mx-5 my-2 duration-150">
+            <Link href={{pathname: "/employee", query: {email: email}}} className="w-fit hover:text-primary mx-5 my-2 duration-150">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-12 h-12">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
                 </svg>
